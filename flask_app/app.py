5a046b5edb2b9d2ad6cee7a41b2479ad82b6b3e9
gglolppc <gglolppc@gmail.com>
2025-07-14 11:06:38 +0300

    fix13

--- flask_app/app.py
+++ flask_app/app.py
@@ -4,24 +4,26 @@ from flask import Flask, g
 from flask_app.routes.auth import auth
 from flask_app.routes.tasks import tasks
 from flask_app.errors.handlers import register_error_handlers
-from flask_app.db.database import  Session
+
 import logging
 from flask_app.db.database import Base, engine
 
+LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs')
+os.makedirs(LOG_DIR, exist_ok=True)
 
-
-print("🚀 Running from:", os.path.abspath(__file__))
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("flask_app/logs/app.log"),
+        logging.FileHandler(os.path.join(LOG_DIR, "app.log")),
         logging.StreamHandler()
     ]
 )
 
 load_dotenv()
-
+print("DATABASE_URL =", os.getenv('DATABASE_URL'))
+if not os.getenv('DATABASE_URL'):
+    raise RuntimeError("DATABASE_URL is not set!")
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.register_blueprint(auth, url_prefix='/', strict_slashes=False)
